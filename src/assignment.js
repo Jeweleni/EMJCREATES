eab5283bf74e056c0644b262ddbc7fcff1aad3da
EMJCREATES <109070607+Jeweleni@users.noreply.github.com>
2022-07-15 20:30:20 +0000

    check challenge 1

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,17 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  
+  let sum = 0;
+
+  for (let i = 0; i < arrayOfNumbers.length;  i++) {
+
+     sum += arrayOfNumbers[i];
+  }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +36,19 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let evenNumberCount = 0;
+
+ for (let i = 0;i < arrayOfNumbers.lenght;i++) {
+   let evenNumber = arrayOfNumber[i] % 2 === 0;
+ 
+    if (evenNumber) {
+      evenNumberCount++;
+       }
+    }
+       return evenNumberCount;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -49,7 +67,7 @@ function countEvenNumbers(arrayOfNumbers) {
 function celsiusToFahrenheit(arrayOfNumbers) {
     return arrayOfNumbers;
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+ assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
